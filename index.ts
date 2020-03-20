05e229aa16c9721f4863fefc266c0eef1c7f2626
Robin Keller <theRobinator@users.noreply.github.com>
2020-03-19 17:53:36 -0700

    Remove questions that aren't needed after SML 2.0

--- index.ts
+++ index.ts
@@ -1,18 +1,14 @@
 import * as Discord from 'discord.js';
 
 
-const doesNotWorkRegexBase = "mods?\\s+((doesn't|don't|does not|not|are not|aren't)\\s+)?((work(ing|s)?|function(al|ing|s)?|compatible|support(ed)?)\\s+)?(in|with|on|for)\\s+(me (on|in)\\s+)?";
 
 // When a message is posted in a supported channel matching the regex, the bot will post the response value.
 // Variable replacements can be used for dynamic messages - see replaceVariables()
 const RESPONSES = new Map<RegExp, string>([
-    [new RegExp(doesNotWorkRegexBase + '(multiplayer|mp)', 'i'), "Hey {{authorMention}}, mods don't work in multiplayer yet. Wait for SML 1.1+ to come out."],
-    [new RegExp(doesNotWorkRegexBase + '(the\\s+)?experimental', 'i'), "Hey {{authorMention}}, mods don't work in experimental yet. Wait for SML 1.1+ to come out, and use Early Access until then."],
     [/missing sid(_p)?/i, 'Did you get a message about missing SID_p from your install? You need to install Satisfactory Item Dictionary from https://ficsit.app/mod/CkUs5KM9ShwVfr'],
     [/(where|how)\s+((can|do)\s+i|to)\s+(find|download|get)(\s+(the|a))?\s+mods?/i, 'Hey {{authorMention}}, you can find mods to download at https://ficsit.app'],
     [/((where|how)\s+((can|do)\s+i|to)|help\s*(me|(me\s*)?(with\s*)?)?)\s+(find(ing)?|download(ing)?|install(ing)?|get(ting)?|put|sav(e|ing))\s+(sml|xinput|satisfactory\s+mod\s+loader)/i, 'Hey {{authorMention}}, check out this video tutorial: https://www.youtube.com/watch?v=OTIIhwZG1Wk'],
     [/(where|how)\s+((can|do)\s+i|to)\s+((start|learn( to)?|get started)\s+)?(modding|mak(e|ing) mods?|mod making)/i, "Hey {{authorMention}}, here are some resources for getting started modding:\nDocumentation: https://docs.ficsit.app\nSatisfactory Headers: https://sf-headers.ficsit.app\nSDK Headers: https://sdk-headers.ficsit.app"],
-    [/(sml|loader|mod|kronos).*(ready|release|work|crash|load|start|error)/i, "Hi {{authorMention}}, mods don't currently work with update 3. The team is working on it, but no release date has been announced yet."],
 ]);
 
 
